0a4feca2611a8ebc86076f76150abc5ffd9e4ac1
rei park <reikop.com@gmail.com>
2015-09-23 01:34:20 +0900

    1.0 제목 버전

--- RefurViewer/ViewController.swift
+++ RefurViewer/ViewController.swift
@@ -8,18 +8,69 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,  UITableViewDelegate, UITableViewDataSource{
+   
+    @IBOutlet var tView: UITableView!
+    
+    
+    var items:NSArray?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+  
+       
+        
+        let url:NSURL! = NSURL(string: "http://reikop.com:8888/mac/fr")!
+        let data = NSData(contentsOfURL: url)
+        
+        
+        do{
+            let json:NSDictionary = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as! NSDictionary
+            items = json["data"] as! NSArray
+            let updateDt:NSTimeInterval = json.valueForKey("update") as! Double
+            let dt = NSDate(timeIntervalSince1970: updateDt / 1000)
+            
+            
+            
+            print(dt);
+//            for item in data{
+//                print(item["title"])
+//            }
+            
+        self.tView.dataSource = self
+        }catch{
+            print(error)
+        }
 
+
+
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    /*func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return swiftBlogs.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = tabView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+        cell.textLabel?.text = "test"
+        return cell;
+    }*/
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+       return (items?.count)!
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        let cell = tView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+        cell.textLabel?.text = items?[indexPath.row]["title"] as? String
+        return cell
+    }
 
+    
 
 }
 
